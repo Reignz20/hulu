45d52e3790bc6d8a00d1cb064d47acd183b90d3b
Roheel <roheelakhtar16@gmail.com>
2021-08-08 12:53:12 +0100

    Second try

--- Components/Header.js
+++ Components/Header.js
@@ -12,7 +12,6 @@ import HeaderItem from './HeaderItem';
 
 function Header() {
 
-
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
             <div className = "flex flex-grow justify-evenly max-w-2xl">
